76956feb6731527ca6ae6c3e62492f68c2e00007
Jhkuno891 <yonervimendoza@gmail.com>
2026-07-28 05:22:39 +0800

    33

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1> started</h1>
+          <h1>Get started</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
@@ -38,7 +38,7 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+          <p>prueba 3</p>
           <ul>
             <li>
               <a href="https://vite.dev/" target="_blank">
